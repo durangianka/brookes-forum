de3eec368c153db3b6d3bc8bf332bad00cdc7727
durangianka <durangianka@gmail.com>
2022-02-20 19:15:31 +0000

    Create client.js

---
+++ pages/client.js
@@ -0,0 +1,13 @@
+import { ref } from "firebase/storage";
+import Header from "../components/Header";
+
+const Client = () => {
+    return (
+        <>
+            <Header />
+            <h1>HELLO THIS IS A CLIENT PAGE</h1>
+        </>
+    )
+}
+
+export default Client;
